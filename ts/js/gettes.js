486c526f9b82453f5c79278b5369407dce3768ff
Vivian <1454902153@qq.com>
2020-02-27 22:24:19 +0800

    hello-world demo

---
+++ ts/js/gettes.js
@@ -0,0 +1,18 @@
+"use strict";
+// function greeter(person: String) {
+//   return 'Hello, ' + person
+// }
+// let user = 'Jane User'
+// document.body.innerHTML = greeter(user)
+// enum Color {
+//   green,
+//   blue = 3,
+//   red = 5
+// // }
+// // console.log(Color.green, Color.blue, Color.red)
+// function justType(): void {
+//   console.log('just want try the void')
+// }
+// let ret: any = justType()
+// console.log(ret)
+// 默认情况下，undefined和null是所有类型的子类型，也就是说可以把null和undefined赋值给任意类型
